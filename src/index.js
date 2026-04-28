af145ee7606cec165e4b2acbcfee6cf9dfdafb67
ar7emid4 <134624325+ar7emid4@users.noreply.github.com>
2026-04-29 00:09:29 +0400

    Add function to switch pizza tab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import { switchTab } from "./switch.js";
+import { pizzaList } from "./pizza-tab/pizza-tab.js";
+
+let pizzaTabButton = document.querySelector("#pizza");
+pizzaTabButton.addEventListener("click", () => {switchTab("Pizza", pizzaList)});
---
+++ src/pizza-tab/pizza-tab.js
@@ -0,0 +1,19 @@
+import hamAndPickles from "./ham-and-pickles.jpg";
+import masalaPizza from "./masala-pizza.jpg";
+import meatFeast from "./meat-feast.jpg";
+import pepperoni from "./pepperoni.jpg";
+import garlicChicken from "./garlic-chicken.jpg";
+import zestySausages from "./zesty-sausages.jpg";
+import fourCheese from "./four-cheese.jpg";
+
+export let pizzaList = [
+    { name: "Ham and pickles", description: "Ham, pickles, red onion, ranch sauce, mozzarella", price: "2.59", image: hamAndPickles },
+    { name: "Masala Pizza", description: "Spicy news: try our new pizza with Indian vibes! It comes with juicy chicken, onion, tomatoes, sweet pepper, alfredo & masala sauces", price: "3.59", image: masalaPizza },
+    { name: "Meat Feast", description: "We've refreshed the recipe: now with five kinds of meat and a new signature sauce that makes the flavor even bolder. Chicken, beef, sausages, pepperoni, and bacon — now even meatier than before!", price: "4.49", image: meatFeast },
+    { name: "Pepperoni", description: "Spicy pepperoni, extra mozzarella cheese, marinara sauce", price: "3.19", image: pepperoni },
+    { name: "Garlic Chicken", description: "Chicken, garlic, tomatoes, mozzarella, Alfredo sauce", price: "2.59", image: garlicChicken },
+    { name: "Zesty Sausages", description: "Classic sausages, red onion, mozzarella, marinara sauce", price: "2.59", image: zestySausages },
+    { name: "Four Cheese", description: "Blue cheese, cheddar cheese, parmesan cheese, mozzarella cheese, Alfredo sauce", price: "3.59", image: fourCheese },
+];
+
+// { name: "", description: "", price: "", image:  },
