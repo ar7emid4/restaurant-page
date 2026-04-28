873262db11b46c0f16aab7528c898ec68b2be352
ar7emid4 <134624325+ar7emid4@users.noreply.github.com>
2026-04-28 22:43:25 +0400

    Edit webpack to deal with CSS

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,12 @@ export default {
   module: {
     rules: [
         {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
         },
     ],
   },
