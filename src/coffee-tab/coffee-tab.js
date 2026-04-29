62f06c8c80c218cb4e60d9f83ad34e88188d41d7
ar7emid4 <134624325+ar7emid4@users.noreply.github.com>
2026-04-29 10:44:41 +0400

    Add coffee tab content

---
+++ src/coffee-tab/coffee-tab.js
@@ -0,0 +1,15 @@
+import iceCoffee from "./ice-coffee.jpg";
+import coldBumbleCoffee from "./cold-bumble-coffee.jpg";
+import iceCappuccino from "./ice-cappuccino.jpg";
+import cappucino from "./cappuccino.jpg";
+import latte from "./latte.jpg";
+import americano from "./americano.jpg";
+
+export let coffeeList = [
+    { name: "Ice Coffee", description: "Everything refreshing is simple: espresso with milk and a few ice cubes", price: "1.69", image: iceCoffee },
+    { name: "Cold Bubmle Coffee", description: "An unusual combination of layers of rich espresso, citrusy orange juice and sweet caramel syrup", price: "1.99", image: coldBumbleCoffee },
+    { name: "Ice Cappuccino", description: "A refreshing drink for coffee lovers. Espresso and ice cream to charge your batteries", price: "2.29", image: iceCappuccino },
+    { name: "Cappucino", description: "The coffee king - classic cappuccino. For a true milk-coffee balance lover", price: "1.69", image: cappucino },
+    { name: "Latte", description: "When you long for a froth, classic latte is here to help", price: "1.69", image: latte },
+    { name: "Americano", description: "A few sips of hot Americano and you're ready to conquer the world", price: "1.29", image: americano },
+];
