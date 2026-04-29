740bed57f1e68472ddc5be9aa77732e64ea80ca3
ar7emid4 <134624325+ar7emid4@users.noreply.github.com>
2026-04-29 11:46:06 +0400

    Add show popup on price button click

--- src/switch.js
+++ src/switch.js
@@ -35,9 +35,69 @@ export function switchTab(tabName, productList) {
             let productButton = document.createElement("button");
             productButton.classList.add("product-button");
             productButton.textContent = `from ${product.price} $`
+            productButton.addEventListener("click", showPopup);
             productButtonContainer.appendChild(productButton);
         }
     }
 
     createCard(productList);
 };
+
+function showPopup () {
+    let popupContainer = document.querySelector("#popup-container");
+    popupContainer.addEventListener("click", () => popupContainer.textContent = "")
+    popupContainer.textContent = "";
+    let popup = document.createElement("div");
+    popup.classList.add("popup");
+
+
+    let popupHeader = document.createElement("div");
+    popupHeader.classList.add("header");
+    popup.appendChild(popupHeader);
+
+    let popupTitleContainer = document.createElement("div");
+    popupHeader.appendChild(popupTitleContainer);
+
+    let popupTitle = document.createElement("h2");
+    popupTitle.classList.add("titile");
+    popupTitle.textContent = "What's your address?";
+    popupTitleContainer.appendChild(popupTitle);
+
+    let closeButton = document.createElement("button");
+    closeButton.classList.add("close-button");
+    closeButton.textContent = "X";
+    closeButton.addEventListener("click", () => popupContainer.textContent = "");
+    popupTitleContainer.appendChild(closeButton);
+
+    let subtitle = document.createElement("h3");
+    subtitle.classList.add("subtitile");
+    subtitle.textContent = "We'll check that you are in the delivery zone and save your address for future orders.";
+    popupHeader.appendChild(subtitle);
+
+
+    let popupActions = document.createElement("div");
+    popupActions.classList.add("actions");
+    popup.appendChild(popupActions);
+
+    let buttonOrange = document.createElement("button");
+    buttonOrange.classList.add("button-orange");
+    buttonOrange.textContent = "Specify the delivery address";
+    popupActions.appendChild(buttonOrange);
+
+    let buttonGray = document.createElement("button");
+    buttonGray.classList.add("button-gray");
+    buttonGray.textContent = "Or Try Takeaway";
+    popupActions.appendChild(buttonGray);
+
+
+    let popupFooter = document.createElement("footer");
+    popupFooter.classList.add("footer");
+    popupFooter.textContent = "Have an account already?";
+    popup.appendChild(popupFooter);
+
+    let popupFooterButton = document.createElement("button");
+    popupFooterButton.classList.add("popup-footer-button");
+    popupFooterButton.textContent = "Log in"
+
+    popupContainer.appendChild(popup);
+}
