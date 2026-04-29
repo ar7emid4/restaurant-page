19aed6e1dc3625968a788cdf81f6132a3b6f7466
ar7emid4 <134624325+ar7emid4@users.noreply.github.com>
2026-04-29 10:17:32 +0400

    Add snacks tab content

---
+++ src/snacks-tab/snacks-tab.js
@@ -0,0 +1,18 @@
+import chorizoBbqPocket from "./chorizo-bbq-pocket-pizza.jpg";
+import meatPasta from "./meat-pasta.jpg";
+import coldChickenRoll from "./cold-chicken-roll.jpg";
+import bakedPotatoes from "./baked-potatoes.jpg";
+import hashbrowns from "./hashbrowns.jpg";
+import chickenBites from "./chicken-bites.jpg";
+import pepperoniOmelette from "./pepperoni-omelette.jpg";
+
+export let snackList = [
+    { name: "Chorizo BBQ Pocket Pizza", description: "Perfect for on-the-go: a hot pocket filled with chorizo sausage, pepperoni, fresh tomatoes, mozzarella, pickles, onions, and burger and barbecue sauces", price: "2.99", image: chorizoBbqPocket },
+    { name: "Meat Pasta", description: "Meet the new Oven-Baked Meat Pasta! It unites spicy beef, juicy chicken, onions, and mozzarella cheese, while creamy Alfredo and a new signature sauce take the flavor balance to the next level", price: "4.25", image: meatPasta },
+    { name: "Cold chicken roll", description: "A chilled snack for a light bite: a roll with chicken, fresh lettuce, tomatoes, cheddar and parmesan, topped with ranch sauce", price: "2.09", image: coldChickenRoll },
+    { name: "Baked potatoes", description: "Hot oven-baked potatoes with seasoning", price: "1.29", image: bakedPotatoes },
+    { name: "Hashbrowns", description: "Oven-baked hash browns. A great snack for breakfast or at any time of the day", price: "1.19", image: hashbrowns },
+    { name: "Chicken Bites", description: "Juicy pieces of golden-brown chicken fillet", price: "2.65", image: chickenBites },
+    { name: "Pepperoni Omelette", description: "Filling and balanced breakfast to go – and oven-baked omelette with pepperoni, tomatoes and mozzarella cheese", price: "2.05", image: pepperoniOmelette },
+];
+// { name: "", description: "", price: "", image:  },
